8475544623c64604ea31cd83ee098087ed67dd29
Лихолетов Михаил <odaving111@gmail.com>
2018-09-21 21:13:05 +0300

    Упражнения по 1 части (простые функции) с переносом строк

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -62,7 +62,7 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String = return if ((age > 10 && age < 20) || (age - age / 10 * 10 >= 0 && age - age / 10 * 10 <= 4)) "$age лет" else
 
 /**
  * Простая
@@ -73,7 +73,7 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double =
 
 /**
  * Простая
